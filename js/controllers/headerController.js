173ad9fdbc22e55987b13313f4046a11b6189e65
admin <admin@192.168.222.67>
2016-10-19 13:42:16 +0200

    animation events capturing test

--- js/controllers/headerController.js
+++ js/controllers/headerController.js
@@ -2,15 +2,19 @@ app.controller('headerController', function ($scope, $animate) {
 
     $scope.entered = false;
 
-    $scope.endAnimateCalendar = function (event) {
-        $scope.animateCalendar = event.animationName == 'calendar' ? true : false;
-        $scope.$apply();
-        console.log('hey');
-    };
+//    $scope.endAnimateCalendar = function (event) {
+//        $scope.animateCalendar = event.animationName == 'calendar' ? true : false;
+//        $scope.$apply();
+//        console.log('hey');
+//    };
 
 //   $animate.addClass(angular.element(document).find('.animate-show-calendar'), '.hide').then(function(){
 //        console.log('promise');
 //   });
 
+
+    $animate.on('removeClass', angular.element(document).find('.animate-show-calendar'), function (element, phase) {
+        console.log(element);
+    });
 });
 
--- js/directives/animationEnd.js
+++ js/directives/animationEnd.js
@@ -1,22 +1,22 @@
-app.directive('animationend', function() {
-	return {
-		restrict: 'A',
-		scope: {
-			animationend: '&'
-		},
-		link: function(scope, element) {
-			var callback = scope.animationend(),
-				  events = 'animationend webkitAnimationEnd MSAnimationEnd' +
-						'transitionend webkitTransitionEnd';
-
-			element.on(events, function(event) {
-				callback.call(element[0], event);
-			});
-			
-                        element.on(events, function(event) {
-				callback.call(element[0], event);
-			});
-		}
-	};
-});
+//app.directive('animationend', function() {
+//	return {
+//		restrict: 'A',
+//		scope: {
+//			animationend: '&'
+//		},
+//		link: function(scope, element) {
+//			var callback = scope.animationend(),
+//				  events = 'animationend webkitAnimationEnd MSAnimationEnd' +
+//						'transitionend webkitTransitionEnd';
+//
+//			element.on(events, function(event) {
+//				callback.call(element[0], event);
+//			});
+//			
+//                        element.on(events, function(event) {
+//				callback.call(element[0], event);
+//			});
+//		}
+//	};
+//});
 
