455ff28b8a2232f84f4432533a0653d4cf050cae
admin <admin@192.168.222.82>
2017-01-16 14:00:00 +0100

    cleaned models

--- back/api/application/models/FinishModel.php
+++ back/api/application/models/FinishModel.php
@@ -7,44 +7,11 @@
  */
 class FinishModel extends CI_Model {
 
-    private $table = 'finish';
-    public $id;
     public $label;
-
-
-    public function get($id = 0) {
-        
-        $result = false;
-        if (!$id) {
-           
-            $result = $this->db->get($this->table);
-        } else {
-            $result = $this->db->get_where($this->table, array('id' => $id), 500, 0);
-        }
-        
-        return $result->result_object();
-    }
-
-    public function save() {
-        if ($this->id < 1) {
-            $this->insert();
-            return $this->db->insert_id();
-        } else {
-            $this->update();
-            return $this->db->affected_rows();
-        }
-    }
-
-    private function insert() {
-        $this->db->insert($this->table, $this);
-    }
-
-    private function update() {
-        $this->db->update($this->table, $this, array('id' => $this));
-    }
     
-    public function delete(){
-       return $this->db->delete($this->table, array('id' => $this->id));
+    public function __construct() {
+        parent::__construct();
+        $this->table = 'finish';
     }
 
 }
