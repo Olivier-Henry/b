46512062f8ba79d539de3c1bca6144d4a2c1289c
Olivier Henry <reivilo2002@hotmail.com>
2017-03-21 22:22:04 +0100

    some styles on image slider

--- adm/js/directives/imageSlider.js
+++ adm/js/directives/imageSlider.js
@@ -2,9 +2,9 @@
 app.directive('imageSlider', function () {
     return {
         restrict: 'E',
-        templateUrl: '../../partials/directives/slider.html',
+        templateUrl: '../adm/partials/directives/slider.html',
         scope: {
-            pictures: '=pictures'
+            pictures: '='
         }
     };
 });
