3cd59a729acc2168283fc4ab18b85a05a729a3a1
Olivier Henry <reivilo2002@hotmail.com>
2017-03-28 13:00:02 +0200

    added Event dialog

--- back/api/application/controllers/Event.php
+++ back/api/application/controllers/Event.php
@@ -7,11 +7,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author olivier
  */
-class Event extends CI_Controller{
-    
+class Event extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
         $this->json = json_decode($this->security->xss_clean($this->input->raw_input_stream));
         $this->load->model('eventmodel');
     }
+
+    public function get($id = 0) {
+        
+    }
+
+    public function save() {
+        if ($this->json) {
+            if (!isset($this->json->id)) {
+                $this->json->id = 0;
+            }
+
+            $this->eventmodel->id = $this->json->id;
+            $this->eventmodel->label = $this->json->label;
+            $this->eventmodel->description = $this->json->description;
+            $this->eventmodel->address = $this->json->address;
+            $this->eventmodel->address_name = $this->json->address_name;
+            $this->eventmodel->date_start = $this->json->date_start;
+            $this->eventmodel->date_end = $this->json->date_end;
+
+            echo json_encode($this->eventmodel->save());
+        }
+    }
+
 }
